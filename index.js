c2f1944638156db30f9eae6b98fefb478e8a6d46
Johnny Zavala <jzavalafgz@gmail.com>
2017-11-14 23:24:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const driver = {};
 function updateDriverWithKeyAndValue(driver, key, value) {
   for (const key in driver) {
     driver[key] = value;
-    return driver[key];
+    alert(typeof driver)
   }
